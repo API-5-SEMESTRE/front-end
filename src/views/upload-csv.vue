cae6d669a9f5b776dd4aecd333c94fb5922378b2
Tairik Johnny <tairikjohnny@gmail.com>
2022-04-12 09:45:09 -0300

    A5S-56 Trabalhando no upload do CSV da empresa

--- src/views/upload-csv.vue
+++ src/views/upload-csv.vue
@@ -184,6 +184,93 @@
                 </v-col>
               </v-row>
             </div>
+            <div class="pt-16">
+              <h1 class="text-center white--text">UPLOAD EMPRESA</h1>
+              <p class="text-center white--text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
+                veritatis illo unde asperiores voluptates voluptatem, enim at
+                ullam, dolore omnis cumque repellendus nesciunt dolores nihil?
+                Praesentium fuga aut optio quam.
+              </p>
+              <v-row class="pa-4">
+                <v-col>
+                  <v-card class="pa-2 rounded-xl" tile outlined color="white">
+                    <v-card-text>
+                      <v-row class="mb-6" no-gutters>
+                        <v-col>
+                          <v-file-input
+                            v-model="filesEmpresa"
+                            color="blue accent-4"
+                            counter
+                            label="Inserir o arquivo"
+                            multiple
+                            placeholder="Inserir o arquivo"
+                            prepend-icon="mdi-paperclip"
+                            outlined
+                            :show-size="1000"
+                          >
+                            <template v-slot:selection="{ index, text }">
+                              <v-chip
+                                v-if="index < 2"
+                                color="blue accent-4"
+                                dark
+                                label
+                                small
+                              >
+                                {{ text }}
+                              </v-chip>
+                              <span
+                                v-else-if="index === 2"
+                                class="overline grey--text text--darken-3 mx-2"
+                              >
+                                +{{ files.length - 2 }} File(s)
+                              </span>
+                            </template>
+                          </v-file-input>
+                        </v-col>
+                      </v-row>
+                      <v-row>
+                        <v-col>
+                          <div id="btn">
+                            <v-form ref="form" v-model="valid" lazy-validation>
+                              <v-btn
+                                :disabled="!valid"
+                                color="primary"
+                                class="mr-4"
+                                @click="sendFileEmpresa"
+                                id="botao-enviar"
+                              >
+                                Enviar
+                              </v-btn>
+                            </v-form>
+                          </div>
+                        </v-col>
+                      </v-row>
+                    </v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <v-card class="mx-auto rounded-xl">
+                    <v-simple-table>
+                      <thead>
+                        <tr>
+                          <th class="text-left">CNPJ</th>
+                          <th class="text-left">Empresa Origem</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr v-for="item in return_empresa" :key="item.id">
+                          <td>{{ item.cnpj }}</td>
+                          <td>{{ item.origem }}</td>
+                        </tr>
+                      </tbody>
+                    </v-simple-table>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </div>
           </v-flex>
         </v-layout>
       </v-container>
@@ -199,11 +286,14 @@ export default {
   data: () => ({
     URL_CIDADE: "/cidade/leitor-csv",
     URL_CNAE: "/cnae/leitor-csv",
+    URL_EMPRESA: "/empresa/leitor-csv",
     valid: true,
     filesCidade: [],
     return_cidade: [],
     filesCnae: [],
     return_cnae: [],
+    filesEmpresa: [],
+    return_empresa: [],
   }),
 
   methods: {
@@ -235,7 +325,6 @@ export default {
           );
         });
     },
-
     sendFileCnae() {
       this.validate();
       const formData = new FormData();
@@ -253,6 +342,28 @@ export default {
           Swal.fire(
             "Oops...",
             "Erro ao importar o arquivo - Erro: " + e.return_cnae.data.error,
+            "error"
+          );
+        });
+    },
+    sendFileEmpresa() {
+      this.validate();
+      const formData = new FormData();
+      for (let file of this.filesEmpresa) {
+        formData.append("arquivo", file, file.name);
+        console
+      }
+      http
+        .post(this.URL_EMPRESA, formData)
+        .then((return_empresa) => {
+          this.return_empresa = return_empresa.data;
+          this.reset();
+          Swal.fire("Sucesso", "Arquivo importado com sucesso", "success");
+        })
+        .catch((e) => {
+          Swal.fire(
+            "Oops...",
+            "Erro ao importar o arquivo - Erro: " + e.return_empresa.data.error,
             "error"
           );
         });
