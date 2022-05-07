a6809e9c56404f94ce2a020c4aaaf8c72b322740
Tairik Johnny <tairikjohnny@pop-os.localdomain>
2022-05-07 14:13:30 -0300

    A5S-74 Refatorando a tela de Upload Empresa

--- src/views/upload-empresa.vue
+++ src/views/upload-empresa.vue
@@ -4,88 +4,87 @@
       <v-container fluid fill-height>
         <v-layout align-center justify-center>
           <v-flex xs12 sm8 md10>
-            <div class="pt-16">
-              <h1 class="text-center white--text">UPLOAD EMPRESA</h1>
+            <div>
+              <h1 class="text-center text-color">UPLOAD EMPRESA</h1>
               <v-row class="pa-4">
                 <v-col>
-                  <v-card class="pa-2 rounded-xl" tile outlined color="white">
-                    <v-card-text>
-                      <v-row class="mb-6" no-gutters>
-                        <v-col>
-                          <v-file-input
-                            v-model="filesEmpresa"
-                            accept="text/csv"
+                  <v-row class="mb-6" no-gutters>
+                    <v-col>
+                      <v-file-input
+                        v-model="filesEmpresa"
+                        accept="text/csv"
+                        color="blue accent-4"
+                        counter
+                        label="Inserir o arquivo"
+                        multiple
+                        placeholder="Inserir o arquivo"
+                        prepend-icon="mdi-paperclip"
+                        outlined
+                        :show-size="1000"
+                        background-color="#C0C0C0"
+                      >
+                        <template v-slot:selection="{ index, text }">
+                          <v-chip
+                            v-if="index < 2"
                             color="blue accent-4"
-                            counter
-                            label="Inserir o arquivo"
-                            multiple
-                            placeholder="Inserir o arquivo"
-                            prepend-icon="mdi-paperclip"
-                            outlined
-                            :show-size="1000"
+                            dark
+                            label
+                            small
                           >
-                            <template v-slot:selection="{ index, text }">
-                              <v-chip
-                                v-if="index < 2"
-                                color="blue accent-4"
-                                dark
-                                label
-                                small
-                              >
-                                {{ text }}
-                              </v-chip>
-                              <span
-                                v-else-if="index === 2"
-                                class="overline grey--text text--darken-3 mx-2"
-                              >
-                                +{{ files.length - 2 }} File(s)
-                              </span>
-                            </template>
-                          </v-file-input>
-                        </v-col>
-                      </v-row>
-                      <v-row>
-                        <v-col>
-                          <div id="btn">
-                            <v-form ref="form" v-model="valid" lazy-validation>
-                              <v-btn
-                                :disabled="!valid"
-                                color="primary"
-                                class="mr-4"
-                                @click="sendFileEmpresa"
-                                id="botao-enviar"
-                              >
-                                Enviar
-                              </v-btn>
-                            </v-form>
-                          </div>
-                        </v-col>
-                      </v-row>
-                    </v-card-text>
-                  </v-card>
+                            {{ text }}
+                          </v-chip>
+                          <span
+                            v-else-if="index === 2"
+                            class="overline grey--text text--darken-3 mx-2"
+                          >
+                            +{{ files.length - 2 }} File(s)
+                          </span>
+                        </template>
+                      </v-file-input>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col>
+                      <div id="btn">
+                        <v-form ref="form" v-model="valid" lazy-validation>
+                          <v-btn
+                            :disabled="!valid"
+                            color="primary"
+                            class="mr-4"
+                            @click="sendFileEmpresa"
+                            id="botao-enviar"
+                          >
+                            Enviar
+                          </v-btn>
+                        </v-form>
+                      </div>
+                    </v-col>
+                  </v-row>
                 </v-col>
               </v-row>
               <v-row>
                 <v-col>
-                  <v-card class="mx-auto rounded-xl">
-                    <v-simple-table>
-                      <thead>
-                        <tr>
-                          <th class="text-left">CNPJ</th>
-                          <th class="text-left">Cidade</th>
-                          <th class="text-left">CNAE</th>
-                          <th class="text-left">Empresa Origem</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr v-for="item in return_empresa" :key="item.id">
-                          <td>{{ item.cnpj }}</td>
-                          <td>{{ item.cidade.descricao }}</td>
-                          <td>{{ item.cnae.descricao }}</td>
-                          <td>{{ item.origem }}</td>
-                        </tr>
-                      </tbody>
-                    </v-simple-table>
+                  <v-card class="pa-3" color="#C0C0C0">
+                    <v-card class="mx-auto">
+                      <v-simple-table>
+                        <thead>
+                          <tr>
+                            <th class="text-left">CNPJ</th>
+                            <th class="text-left">Cidade</th>
+                            <th class="text-left">CNAE</th>
+                            <th class="text-left">Empresa Origem</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr v-for="item in return_empresa" :key="item.id">
+                            <td>{{ item.cnpj }}</td>
+                            <td>{{ item.cidade.descricao }}</td>
+                            <td>{{ item.cnae.descricao }}</td>
+                            <td>{{ item.origem }}</td>
+                          </tr>
+                        </tbody>
+                      </v-simple-table>
+                    </v-card>
                   </v-card>
                 </v-col>
               </v-row>
@@ -145,7 +144,7 @@ export default {
 
 <style scoped>
 #upload-csv {
-  background-color: #0d1b2a;
+  background-color: white;
 }
 #botao-enviar {
   margin-top: 20px;
