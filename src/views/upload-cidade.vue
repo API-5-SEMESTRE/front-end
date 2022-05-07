3edd9fe94616a0dc642475b72337e4a14310c83d
Tairik Johnny <tairikjohnny@pop-os.localdomain>
2022-05-07 13:59:21 -0300

    A5S-72 Refatorando a tela de Upload Cidade

--- src/views/upload-cidade.vue
+++ src/views/upload-cidade.vue
@@ -5,87 +5,86 @@
         <v-layout align-center justify-center>
           <v-flex xs12 sm8 md10>
             <div>
-              <h1 class="text-center white--text">UPLOAD CIDADE</h1>
+              <h1 class="text-center text-color">UPLOAD CIDADE</h1>
               <v-row class="pa-4">
                 <v-col>
-                  <v-card class="pa-2 rounded-xl" tile outlined color="white">
-                    <v-card-text>
-                      <v-row class="mb-6" no-gutters>
-                        <v-col>
-                          <v-file-input
-                            v-model="filesCidade"
-                            accept="text/csv"
+                  <v-row class="mb-6" no-gutters>
+                    <v-col>
+                      <v-file-input
+                        v-model="filesCidade"
+                        accept="text/csv"
+                        color="blue accent-4"
+                        counter
+                        label="Inserir o arquivo"
+                        multiple
+                        placeholder="Inserir o arquivo"
+                        prepend-icon="mdi-paperclip"
+                        outlined
+                        :show-size="1000"
+                        background-color="#C0C0C0"
+                      >
+                        <template v-slot:selection="{ index, text }">
+                          <v-chip
+                            v-if="index < 2"
                             color="blue accent-4"
-                            counter
-                            label="Inserir o arquivo"
-                            multiple
-                            placeholder="Inserir o arquivo"
-                            prepend-icon="mdi-paperclip"
-                            outlined
-                            :show-size="1000"
+                            dark
+                            label
+                            small
                           >
-                            <template v-slot:selection="{ index, text }">
-                              <v-chip
-                                v-if="index < 2"
-                                color="blue accent-4"
-                                dark
-                                label
-                                small
-                              >
-                                {{ text }}
-                              </v-chip>
-                              <span
-                                v-else-if="index === 2"
-                                class="overline grey--text text--darken-3 mx-2"
-                              >
-                                +{{ files.length - 2 }} File(s)
-                              </span>
-                            </template>
-                          </v-file-input>
-                        </v-col>
-                      </v-row>
-                      <v-row>
-                        <v-col>
-                          <div id="btn">
-                            <v-form ref="form" v-model="valid" lazy-validation>
-                              <v-btn
-                                :disabled="!valid"
-                                color="primary"
-                                class="mr-4"
-                                @click="sendFileCidade"
-                                id="botao-enviar"
-                              >
-                                Enviar
-                              </v-btn>
-                            </v-form>
-                          </div>
-                        </v-col>
-                      </v-row>
-                    </v-card-text>
-                  </v-card>
+                            {{ text }}
+                          </v-chip>
+                          <span
+                            v-else-if="index === 2"
+                            class="overline grey--text text--darken-3 mx-2"
+                          >
+                            +{{ files.length - 2 }} File(s)
+                          </span>
+                        </template>
+                      </v-file-input>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col>
+                      <div id="btn">
+                        <v-form ref="form" v-model="valid" lazy-validation>
+                          <v-btn
+                            :disabled="!valid"
+                            color="primary"
+                            class="mr-4"
+                            @click="sendFileCidade"
+                            id="botao-enviar"
+                          >
+                            Enviar
+                          </v-btn>
+                        </v-form>
+                      </div>
+                    </v-col>
+                  </v-row>
                 </v-col>
               </v-row>
               <v-row>
                 <v-col>
-                  <v-card class="mx-auto rounded-xl">
-                    <v-simple-table>
-                      <thead>
-                        <tr>
-                          <th class="text-left">Id</th>
-                          <th class="text-left">Cidade</th>
-                          <th class="text-left">UF</th>
-                          <th class="text-left">Região</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr v-for="item in return_cidade" :key="item.id">
-                          <td>{{ item.id }}</td>
-                          <td>{{ item.descricao }}</td>
-                          <td>{{ item.siglaEstado }}</td>
-                          <td>{{ item.registroIbge }}</td>
-                        </tr>
-                      </tbody>
-                    </v-simple-table>
+                  <v-card class="pa-3" color="#C0C0C0">
+                    <v-card class="mx-auto">
+                      <v-simple-table>
+                        <thead>
+                          <tr>
+                            <th class="text-left">Id</th>
+                            <th class="text-left">Cidade</th>
+                            <th class="text-left">UF</th>
+                            <th class="text-left">Região</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr v-for="item in return_cidade" :key="item.id">
+                            <td>{{ item.id }}</td>
+                            <td>{{ item.descricao }}</td>
+                            <td>{{ item.siglaEstado }}</td>
+                            <td>{{ item.registroIbge }}</td>
+                          </tr>
+                        </tbody>
+                      </v-simple-table>
+                    </v-card>
                   </v-card>
                 </v-col>
               </v-row>
@@ -144,7 +143,10 @@ export default {
 
 <style scoped>
 #upload-csv {
-  background-color: #0d1b2a;
+  background-color: white;
+}
+.text-color {
+  color: #274c77;
 }
 #botao-enviar {
   margin-top: 20px;
