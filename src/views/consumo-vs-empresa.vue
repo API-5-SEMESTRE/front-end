8f43817acc2b2f4f7df56e71b8d76af42072a00f
Tairik Johnny <tairikjohnny@pop-os.localdomain>
2022-05-11 16:55:02 -0300

    Inserindo o grafico de Consumo VS Empresa na página Consumo VS Empresa

--- src/views/consumo-vs-empresa.vue
+++ src/views/consumo-vs-empresa.vue
@@ -7,57 +7,88 @@
             <div>
               <v-row>
                 <v-col>
-                  <h1 class="text-center text-color">CONSUMO VS EMPRESA</h1>
-                  <v-row class="pa-4">
-                    <v-col>
-                      <v-row class="mb-6" no-gutters>
-                        <v-col>
-                          <v-text-field
-                            label="CNPJ"
-                            v-model="consumo1.cnpj"
-                            :rules="regra_cnpj"
-                            single-line
-                            solo
-                            required
-                            dense
-                            background-color="#e0e1dd"
-                            max-width="500"
-                          ></v-text-field>
-                        </v-col>
-                      </v-row>
-                      <v-row>
-                        <v-col>
-                          <div id="btn">
-                            <v-form ref="form" lazy-validation>
-                              <v-btn
-                                color="primary"
-                                class="mr-4"
-                                @click="gerarGraficoConsumoVsCnpjPDF(consumo1)"
-                                id="botao-enviar"
-                              >
-                                Download PDF
-                              </v-btn>
-                            </v-form>
-                          </div>
-                        </v-col>
-                        <v-col>
-                          <div id="btn">
-                            <v-form ref="form" lazy-validation>
-                              <v-btn
-                                color="primary"
-                                class="mr-4"
-                                @click="gerarGraficoConsumoVsCnpjPNG(consumo1)"
-                                id="botao-enviar"
-                              >
-                                Download PNG
-                              </v-btn>
-                            </v-form>
-                          </div>
-                        </v-col>
-                      </v-row>
-                    </v-col>
-                  </v-row>
+                  <v-card width="600" class="mx-auto">
+                    <h1 class="text-center text-color">CONSUMO VS EMPRESA</h1>
+                    <v-row class="pa-4">
+                      <v-col>
+                        <v-row class="mb-6" no-gutters>
+                          <v-col>
+                            <v-text-field
+                              label="CNPJ"
+                              v-model="consumo1.cnpj"
+                              single-line
+                              solo
+                              required
+                              dense
+                              background-color="#e0e1dd"
+                              width="200"
+                            ></v-text-field>
+                          </v-col>
+                        </v-row>
+                        <v-row>
+                          <v-col>
+                            <div id="btn">
+                              <v-form ref="form" lazy-validation>
+                                <v-btn
+                                  color="primary"
+                                  class="mr-4"
+                                  @click="
+                                    gerarGraficoConsumoVsCnpjFront(consumo1)
+                                  "
+                                  id="botao-enviar"
+                                >
+                                  Gerar Gráfico
+                                </v-btn>
+                              </v-form>
+                            </div>
+                          </v-col>
+                          <v-col>
+                            <div id="btn">
+                              <v-form ref="form" lazy-validation>
+                                <v-btn
+                                  color="primary"
+                                  class="mr-4"
+                                  @click="
+                                    gerarGraficoConsumoVsCnpjPDF(consumo1)
+                                  "
+                                  id="botao-enviar"
+                                >
+                                  Download PDF
+                                </v-btn>
+                              </v-form>
+                            </div>
+                          </v-col>
+                          <v-col>
+                            <div id="btn">
+                              <v-form ref="form" lazy-validation>
+                                <v-btn
+                                  color="primary"
+                                  class="mr-4"
+                                  @click="
+                                    gerarGraficoConsumoVsCnpjPNG(consumo1)
+                                  "
+                                  id="botao-enviar"
+                                >
+                                  Download PNG
+                                </v-btn>
+                              </v-form>
+                            </div>
+                          </v-col>
+                        </v-row>
+                      </v-col>
+                    </v-row>
+                  </v-card>
                 </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <LineChart
+                    :lista_consumo="this.lista_quantidade_consumo"
+                    :lista_mes="this.lista_mes_referencia"
+                  />
+                </v-col>
+              </v-row>
+              <v-row>
                 <v-col>
                   <h1 class="text-center text-color">CONSUMO VS 2 EMPRESAS</h1>
                   <v-row class="pa-4">
@@ -67,7 +98,6 @@
                           <v-text-field
                             label="CNPJ 1"
                             v-model="consumo2.cnpj1"
-                            :rules="regra_cnpj"
                             single-line
                             solo
                             required
@@ -82,7 +112,6 @@
                           <v-text-field
                             label="CNPJ 2"
                             v-model="consumo2.cnpj2"
-                            :rules="regra_cnpj"
                             single-line
                             solo
                             required
@@ -93,7 +122,7 @@
                         </v-col>
                       </v-row>
                       <v-row>
-                          <v-col>
+                        <v-col>
                           <div id="btn">
                             <v-form ref="form" lazy-validation>
                               <v-btn
@@ -126,9 +155,6 @@
                   </v-row>
                 </v-col>
               </v-row>
-              <v-row>
-                <v-col> </v-col>
-              </v-row>
             </div>
           </v-flex>
         </v-layout>
@@ -140,9 +166,16 @@
 <script>
 import axios from "axios";
 import Swal from "sweetalert2";
+import LineChart from "../components/Line.vue";
+import Axios from "axios";
 
 export default {
+  components: { LineChart },
   data: () => ({
+    // Variaveis do grafico de Consumo VS Empresa
+    lista_quantidade_consumo: [],
+    lista_mes_referencia: [],
+
     consumo1: {
       cnpj: "",
     },
@@ -154,6 +187,23 @@ export default {
   }),
 
   methods: {
+    gerarGraficoConsumoVsCnpjFront(consumo1) {
+      Axios({
+        url: `http://localhost:8080/consumo/lista-consumo-empresa/${consumo1.cnpj}`,
+        method: "GET",
+      }).then((response) => {
+        response.data.forEach((item) => {
+          this.lista_quantidade_consumo.push(item.quantidadeConsumo);
+        });
+        console.log(this.lista_quantidade_consumo);
+        response.data.forEach((item) => {
+          this.lista_mes_referencia.push(item.mesReferencia);
+        });
+        console.log(this.lista_mes_referencia);
+
+        this.loaded = true;
+      });
+    },
     gerarGraficoConsumoVsCnpjPNG(consumo1) {
       axios({
         url: `http://localhost:5000/graph/consumo/${consumo1.cnpj}`,
